50bf28ca4a7ca10354bacfe70cb91eca096afef3
VaishnaviRajput <rajputvaishnavikaransing10@gmail.com>
2024-06-21 21:21:18 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
   return (
-    <div id="main">
-               {/* Do not remove the main div */}
+    <div>
+      <h1>Relatives to Visit This Diwali</h1>
+      <ol key="relativeList">
+        <li key="relativeListItem1">Uncle Ramesh</li>
+        <li key="relativeListItem2">Aunt Meera</li>
+        <li key="relativeListItem3">Cousin Priya</li>
+        <li key="relativeListItem4">Grandma Kamala</li>
+        <li key="relativeListItem5">Brother Rahul</li>
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
